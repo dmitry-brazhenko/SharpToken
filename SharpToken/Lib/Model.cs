2aa09dbaf31b3a4e8783f0417316e5d77d94baad
dmitry-brazhenko <brazhenko.dmitry@gmail.com>
2024-02-15 13:21:26 +0100

    text-embedding-3-small", "cl100k_base

--- SharpToken/Lib/Model.cs
+++ SharpToken/Lib/Model.cs
@@ -37,6 +37,7 @@ namespace SharpToken
             // embeddings
             { "text-embedding-ada-002", "cl100k_base" },
             { "text-embedding-3-large", "cl100k_base" },
+            { "text-embedding-3-small", "cl100k_base" },
             // old embeddings
             { "text-similarity-davinci-001", "r50k_base" },
             { "text-similarity-curie-001", "r50k_base" },
