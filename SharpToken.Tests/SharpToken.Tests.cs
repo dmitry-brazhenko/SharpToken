5cf058bb6ae409f39d3ec999c6bae03ae3bcc90f
Tom Winzig <thomas@winzig.com>
2024-05-15 12:24:16 -0700

    Fixing typo
    Accidentally removed a using statement in my last update

--- SharpToken.Tests/SharpToken.Tests.cs
+++ SharpToken.Tests/SharpToken.Tests.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using System.Text;
 using NUnit.Framework;
 
@@ -68,6 +69,7 @@ public class Tests
             });
         }
     }
+
 
     [Test]
     public void TestEncodingWithCustomAllowedSet()
